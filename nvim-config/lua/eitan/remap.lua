5d672b5c77842f6e0afc3812412db7aae1ea334a
Eitan Lees <eitanlees@gmail.com>
2023-04-06 08:57:23 -0400

    Settings Touch-up

--- nvim-config/lua/eitan/remap.lua
+++ nvim-config/lua/eitan/remap.lua
@@ -1,11 +1,11 @@
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", vim.lsp.buf.format)
 
--- I want to add a keymap which inserts the 
--- date but I have not been able to figure 
+-- I want to add a keymap which inserts the
+-- date but I have not been able to figure
 -- it out yet. Ugh!
 --
 -- This is the dream
 -- :r!date "+\%F"
--- or 
+-- or
 -- :put=system('date +%F')<CR>
-
--- nvim-config/lua/eitan/set.lua
+++ nvim-config/lua/eitan/set.lua
@@ -3,11 +3,26 @@ vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 
-vim.hlsearch = false
-vim.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 4
+
+vim.opt.updatetime = 50
+
+vim.opt.signcolumn = "yes"
 
 vim.g.netrw_banner = 0
